2622aac3eed845a49de69c5df18a3105857c1f31
deathkryz <deathkryz@gmail.com>
2023-04-28 23:02:20 -0700

    Javascript para envio de mensaje

--- js/site.js
+++ js/site.js
@@ -10,27 +10,27 @@ jQuery(document).ready(function($){
 	$('.img-holder').imageScroll();
 	
 	// Scrollbar 
-	if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
-	window.onmousewheel = document.onmousewheel = wheel;
+	// if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+	// window.onmousewheel = document.onmousewheel = wheel;
+	//
+	// function wheel(event) {
+	// 	var delta = 0;
+	// 	if (event.wheelDelta) delta = event.wheelDelta / 120;
+	// 	else if (event.detail) delta = -event.detail / 3;
+	//
+	// 	handle(delta);
+	// 	if (event.preventDefault) event.preventDefault();
+	// 	event.returnValue = false;
+	// }
 	 
-	function wheel(event) {
-		var delta = 0;
-		if (event.wheelDelta) delta = event.wheelDelta / 120;
-		else if (event.detail) delta = -event.detail / 3;
-	 
-		handle(delta);
-		if (event.preventDefault) event.preventDefault();
-		event.returnValue = false;
-	}
-	 
-	function handle(delta) {
-		var time = 800; // delay time
-		var distance = 350; // delta point 
-		// Dom where it will apply 
-		$('html, body').stop().animate({
-			scrollTop: $(window).scrollTop() - (distance * delta)
-		}, time );
-	}
+	// function handle(delta) {
+	// 	var time = 800; // delay time
+	// 	var distance = 350; // delta point
+	// 	// Dom where it will apply
+	// 	$('html, body').stop().animate({
+	// 		scrollTop: $(window).scrollTop() - (distance * delta)
+	// 	}, time );
+	// }
 	
 	// Contact Form
 	$('#forms').submit(function(e){
